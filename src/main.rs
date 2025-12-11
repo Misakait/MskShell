495910b4f2363bad066339eb2e22760cf94ef8bf
misakait <cjl2597588144@gmail.com>
2025-12-11 20:00:58 +0800

    Prompt in a loop and read commands

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,13 @@
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
-<<<<<<< HEAD
-    let mut command;
-=======
-    let mut command = String::new();
->>>>>>> 5dbe443 (Read user input and report unknown commands)
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut command).unwrap();
-    match command.trim() {
-        _ => println!("{}: command not found", command.trim()),
+    loop {
+        print!("$ ");
+        let mut command = String::new();
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut command).unwrap();
+        match command.trim() {
+            _ => println!("{}: command not found", command.trim()),
+        }
     }
 }
