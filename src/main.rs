ef50b3fac144cf2e989a2eb62144e1a81c254641
misakait <cjl2597588144@gmail.com>
2025-12-11 19:57:10 +0800

    Read user input and report unknown commands

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,11 @@ use std::io::{self, Write};
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
+<<<<<<< HEAD
     let mut command;
+=======
+    let mut command = String::new();
+>>>>>>> 5dbe443 (Read user input and report unknown commands)
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut command).unwrap();
     match command.trim() {
